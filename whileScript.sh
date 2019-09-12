a66937493f0b0b7b70542ad30ce5817dea133727
derozo <derozo@gmail.com>
2019-09-12 19:29:16 -0300

    Reverting!!! Revert "Commit with first change on Master"
    This reverts commit c75fb13f7ab32491a6c14ac5d6890ffed33143cf.

--- whileScript.sh
+++ whileScript.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+<<<<<<< HEAD
 #cambio solo para ver en git
 
+#This change will be reverted
 
 echo "Welcome to Messages Branch"
 
+=======
+>>>>>>> parent of c75fb13... Commit with first change on Master
 i=0
 while [ $i -lt 10 ]
 do
