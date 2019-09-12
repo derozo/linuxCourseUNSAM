290945467a5671db5a1f0a96b5808ff46e76258d
derozo <derozo@gmail.com>
2019-09-12 19:49:09 -0300

    Revert "Reverting!!!" with no confilcts
    This reverts commit a66937493f0b0b7b70542ad30ce5817dea133727.

--- whileScript.sh
+++ whileScript.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#cambio solo para ver en git again
 
-#This change will be reverted
+#cambio solo para ver en git again after solve conflict on file
 
 echo "Welcome to Messages Branch"
 
-=======
->>>>>>> parent of c75fb13... Commit with first change on Master
 i=0
 while [ $i -lt 10 ]
 do
